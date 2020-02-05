de57cb736c73a9e09c67ba3fce3504bbd8178ac1
Ira <ira.d.sanchez@gmail.com>
2020-02-05 13:43:54 -0700

    Add Completed Stock Prices
    - Definitely could be more DRY. My nested if's are listed 2x.

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -9,28 +9,34 @@ import argparse
 
 
 def find_max_profit(prices):
-    # goal: find the maximum profit that can be made from a single buy and then sell of these stock prices.
-    # 1. UNDERSTAND
-    #                         buy        sell                            sell-buy=max profit
-    #                          ⬇           ⬇                               ⬇   ⬇    ⬇
-    # `find_max_profit([1050, 270, 1540, 3800, 2])` should return 3530 # 3800-270=3530,
-    #                                          ⬆ not 2 b/c it comes after 3800
-    # !! This means I CAN'T change the order in relation to each other.
-    #
-    # the current min price is automatically the first thing
     current_min_price_so_far = prices[0]
-    max_profit_so_far = 0
 
-    # I need to search, so I need to iterate or recurse, i can skip the first thing, since i stored it already
+    # ⬇ can't use a number cause what if the possible profit is actually a loss?
+    max_profit_so_far = None
+
     for i in range(1, len(prices)):
-        # check if this is the min price
+        possible_max = prices[i] - current_min_price_so_far
+        # handle smaller values
         if prices[i] < current_min_price_so_far:
+            # if max is none reset it
+            if max_profit_so_far is None:
+                max_profit_so_far = prices[i] - current_min_price_so_far
+            # if this is the new max, reset it
+            if possible_max > max_profit_so_far:
+                max_profit_so_far = prices[i] - current_min_price_so_far
+            # set the min price if the index is smaller than the current min price
             current_min_price_so_far = prices[i]
-            print(current_min_price_so_far)
-        # set the max profit
+        # handle larger values
+        if prices[i] > current_min_price_so_far:
+            # if max is none reset it
+            if max_profit_so_far is None:
+                max_profit_so_far = prices[i] - current_min_price_so_far
+            # set the max profit if there's a new one
+            elif possible_max > max_profit_so_far:
+                max_profit_so_far = prices[i] - current_min_price_so_far
 
+    return max_profit_so_far
 
-find_max_profit([1050, 270, 1540, 3800, 2])
 
 if __name__ == '__main__':
     # This is just some code to accept inputs from the command line
